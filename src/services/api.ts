92c2b00d8378df3143a3a35ac7eb88f5ec65c7e1
Victor Hugo <csivictorhugo@gmail.com>
2021-04-26 16:30:42 -0300

    default

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,6 +1,5 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  // baseURL: process.env.API_URL || 'http://localhost:3333'
-  baseURL: 'https://podcastr-app.s3-sa-east-1.amazonaws.com/server.json'
+  baseURL: process.env.API_URL || 'http://localhost:3333'
 })

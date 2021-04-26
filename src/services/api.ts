3f801c8d0a3a7da9ced598f3f12cb181031def59
Victor Hugo <csivictorhugo@gmail.com>
2021-04-26 08:18:16 -0300

    fake api

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,6 @@
 import axios from 'axios';
 
 export const api = axios.create({
-  baseURL: process.env.API_URL || 'http://localhost:3333'
+  baseURL: 'https://raw.githubusercontent.com/itsmevictorhugo/podcastr/master/server.json'
+  // baseURL: process.env.API_URL || 'http://localhost:3333'
 })
